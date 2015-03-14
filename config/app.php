c1a55f50508b70287d38b1c48951b334d631dd99
vincent <oooooee@qq.com>
2015-03-14 20:45:27 +0800

    恢复

--- config/app.php
+++ config/app.php
@@ -137,7 +137,7 @@ return [
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Html\HtmlServiceProvider',
 
-//		'Barryvdh\Debugbar\ServiceProvider',
+		'Barryvdh\Debugbar\ServiceProvider',
 
 		/*
 		 * Application Service Providers...
