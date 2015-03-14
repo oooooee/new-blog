2a086778304b401ebfc9f2deaeae5ac3dc07e033
vincent <oooooee@qq.com>
2015-03-14 15:26:00 +0800

    修改数据库配置

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -8,9 +8,9 @@ class UsersTableSeeder extends Seeder {
 	public function run()
 	{
 		\App\User::create([
-			'username' => 'ganto',
-			'email' => 'ganto@qq.com',
-			'password' => Hash::make('123456')
+			'username' => 'Coke-Vincent',
+			'email' => 'Coke-Vincent@qq.com',
+			'password' => Hash::make('h910528')
 		]);
 
 	}
