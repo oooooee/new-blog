e9ccaa0a5498a72f713f0f03978a0c64734828fc
ooooee <oooooee@qq.com>
2015-03-25 11:29:05 +0800

    增加没得比数据抓取

--- app/Http/Controllers/SpiderController.php
+++ app/Http/Controllers/SpiderController.php
@@ -10,7 +10,7 @@ class SpiderController extends Controller {
      * 匹配正则
      * @var string
      */
-    private $pattern = '/(花王|贝亲|笔记本|卡西欧|纸巾|机械.*?键盘|gxg|新百伦|施巴|妙思乐|维达|人字拖)/';
+    private $pattern = '/(花王|贝亲|笔记本|卡西欧|纸巾|机械.*?键盘|gxg|新百伦|施巴|妙思乐|维达|人字拖|化石)/';
 
     private function is_match($str){
         if (preg_match($this->pattern, $str)) {
@@ -47,7 +47,7 @@ class SpiderController extends Controller {
 
         $huihui = [];
 
-        $meidebi = [];
+        $meidebi = $this->meidebi();
 
         $this->send_mail(array_merge($mgpyh, $smzdm, $huihui, $meidebi));
     }
@@ -136,4 +136,41 @@ class SpiderController extends Controller {
 
 
 	}
+
+    public function meidebi(){
+
+        // 这是手机的api接口
+        $url = 'http://a.meidebi.com/Share-allhotlist?limit=50&p=1';
+
+        $curl = new Curl();
+        $content = $curl->get($url);
+        $content = json_decode($content->body, true);
+        $content = $content['data']['linklist'];
+
+        $spider = new Spider();
+        $mail_rows = [];
+        print_r($content);
+        foreach ($content as $k => $v) {
+            if ($this->is_match($v['title'])) {
+                $id = 'meidebi_' . $v['id'];
+                $insert_data = [
+                    'id' => $id,
+                    'title' => $v['title'],
+                    'content' => '',
+                    'url' => $v['orginurl'],
+                    'post_at' => date('Y-m-d H:i:s', $v['createtime']),
+                    'category' => $v['categoryname'],
+                    'from' => 'meidebi',
+                    'is_send' => 1,
+                ];
+                if ($spider->add_new($insert_data)) {
+                    $mail_rows[] = $insert_data;
+                }
+            }
+        }
+        print_r($mail_rows);
+
+        return $mail_rows;
+
+    }
 }
